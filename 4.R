afe05801b601118d76efcf890dea48a6c69ba50d
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-24 19:30:46 -0500

    c4

--- 4.R
+++ 4.R
@@ -1,3 +1,3 @@
 shrub_volume <- read_csv("./data/shrub-volume-experiments-table.csv")
-print(shrub_data_manipulations <  inner_join(shrub_dimensions, shrub_volume_experiments_table, by=NULL))
+print(shrub_data_manipulations <  inner_join(shrub_dimensions, shrub_volume, by=NULL))
 
